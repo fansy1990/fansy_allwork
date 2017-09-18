16e5ec8f938f114ee0d5e87f3952839ad40ca7c0
fansy <fansy1990@foxmail.com>
2017-09-18 13:49:36 +0800

    添加Hadoop 基础配置

---
+++ common/src/main/java/common/utils/HadoopUtil.java
@@ -0,0 +1,47 @@
+package common.utils;
+
+import org.apache.hadoop.conf.Configuration;
+
+import java.util.Map;
+
+/**
+ * Hadoop 工具类
+ * Created by fansy on 2017/9/18.
+ */
+public class HadoopUtil {
+    private static final String hadoop_properties ="/hadoop.properties";
+    private static Configuration conf = null;
+    public static Configuration getConf() {
+        if (conf == null) {
+            Map<String,String> confMap = PropertiesUtil.getProperties(hadoop_properties);
+            conf = new Configuration();
+            conf.setBoolean("mapreduce.app-submission.cross-platform", Boolean
+                    .parseBoolean(confMap
+                            .get("mapreduce.app-submission.cross-platform")));// 配置使用跨平台提交任务
+            conf.set("fs.defaultFS", confMap.get("fs.defaultFS"));// 指定namenode
+            conf.set("mapreduce.framework.name",
+                    confMap.get("mapreduce.framework.name")); // 指定使用yarn框架
+            conf.set("yarn.resourcemanager.address",
+                    confMap.get("yarn.resourcemanager.address")); // 指定resourcemanager
+            conf.set("yarn.resourcemanager.scheduler.address",
+                    confMap.get("yarn.resourcemanager.scheduler.address"));// 指定资源分配器
+            conf.set("mapreduce.jobhistory.address",
+                    confMap.get("mapreduce.jobhistory.address"));// 指定historyserver
+//            conf.set("mapreduce.job.jar", JarUtil.jar(JarUtil.class));// 设置jar包路径
+        }
+        return conf;
+    }
+
+    /**
+     * 向Configuration里面添加配置
+     * 比如，更新mapreduce.job.jar的配置
+     * @param key
+     * @param value
+     */
+    public static void addPropertiesToConfiguration(String key,String value){
+        if(conf == null){
+            getConf();
+        }
+        conf.set(key,value);
+    }
+}
---
+++ common/src/main/java/common/utils/JarUtil.java
@@ -0,0 +1,94 @@
+package common.utils;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.jar.JarEntry;
+import java.util.jar.JarOutputStream;
+
+/**
+ * 自动打包算法包
+ * Created by fansy on 2017/9/18.
+ */
+public class JarUtil {
+    public static final Logger log = LoggerFactory.getLogger(JarUtil.class);
+    private static String jarFilePath = null;
+
+    private static void jar(String inputFileName, String outputFileName) {
+        JarOutputStream out = null;
+        try {
+            out = new JarOutputStream(new FileOutputStream(outputFileName));
+            File f = new File(inputFileName);
+            jar(out, f, "");
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                out.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    /**
+     * 把classes目录下面的文件打jar包，并返回jar包路径
+     *
+     * @param cls
+     * @return
+     */
+    public static String jar(Class<?> cls) {
+        String outputJar = cls.getClassLoader().getResource("/").getFile()
+                .replace("WEB-INF/classes/", "")
+                + "hdfs2hbase.jar";
+        return jar(cls,outputJar);
+    }
+
+    /**
+     * 指定输出路径
+     * @param cls
+     * @param outputJar
+     * @return
+     */
+    public static String jar(Class<?> cls,String outputJar){
+        jar(cls.getClassLoader().getResource("").getFile(), outputJar);
+        jarFilePath = outputJar;
+        log.info("MR jar:" + outputJar);
+        return outputJar;
+    }
+
+    private static void jar(JarOutputStream out, File f, String base)
+            throws Exception {
+        if (f.isDirectory()) {
+            File[] fl = f.listFiles();
+            base = base.length() == 0 ? "" : base + "/"; // 注意，这里用左斜杠
+            for (int i = 0; i < fl.length; i++) {
+                jar(out, fl[i], base + fl[i].getName());
+            }
+        } else {
+            out.putNextEntry(new JarEntry(base));
+            FileInputStream in = new FileInputStream(f);
+            byte[] buffer = new byte[1024];
+            int n = in.read(buffer);
+            while (n != -1) {
+                out.write(buffer, 0, n);
+                n = in.read(buffer);
+            }
+            in.close();
+        }
+    }
+
+    public static String getJarFilePath() {
+        return jarFilePath;
+    }
+
+    public static void setJarFilePath(String jarFilePath) {
+        JarUtil.jarFilePath = jarFilePath;
+    }
+
+}
